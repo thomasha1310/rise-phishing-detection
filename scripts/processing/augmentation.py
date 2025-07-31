69b7bd4da6175560bb5279cdcdcd8caa40de130f
Thomas Ha <thomasha.1310@gmail.com>
2025-07-31 14:30:00 -0400

    Remove temp.py

--- scripts/processing/augmentation.py
+++ scripts/processing/augmentation.py
@@ -123,10 +123,10 @@ df['body_no_stopwords'] = df['body'].apply(
         if word.isalnum() and not word.isnumeric() and word not in STOP_WORDS
     )
 )
-df.dropna(inplace=True)
 
 # ========== EXPORT DATA ==========
 
+df.dropna(inplace=True)
 print("Exporting data...")
 df.to_csv('./data/analysis/emails_augmented.csv', index=False)
 print("Done.")
